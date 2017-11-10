f311553801cdddc0e8512da0632c412714056910
DESKTOP-DMGLT16\Ela <ELa>
2017-11-10 11:21:31 +0100

    Second commit

---
+++ src/javaswing/DatabaseViewer.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaswing;
+
+import java.awt.BorderLayout;
+import static java.awt.BorderLayout.EAST;
+import static java.awt.BorderLayout.WEST;
+import java.awt.Container;
+import javax.swing.Icon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author Ela
+ */
+public class DatabaseViewer extends JFrame {
+    private JButton czysc;
+    private JButton wykonaj;
+    
+    private JPanel addPanel;
+    
+    private JLabel textGora;
+    private JTextArea textDol;
+
+   
+    public void initComponents(){
+        
+        addPanel = new JPanel();
+        this.add(addPanel);
+        
+        this.setTitle("DatabaseViewer");
+
+        //do Rafala - jak przekazac np BorderLayout jako parametr?
+        czysc = new JButton("Wyczysc");   
+        this.add(czysc, BorderLayout.WEST);
+      
+        wykonaj = new JButton("Wykonaj");   
+        this.add(wykonaj, BorderLayout.EAST);
+        
+        textGora = new JLabel();
+        this.add(textGora, BorderLayout.NORTH);
+        
+        textDol = new JTextArea();
+        this.add(textDol, BorderLayout.SOUTH);
+        
+        
+
+
+
+        this.setSize(500,500);
+        this.setVisible(true);
+    }
+}
--- src/javaswing/JavaSwing.java
+++ src/javaswing/JavaSwing.java
@@ -15,7 +15,16 @@ public class JavaSwing {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        java.awt.EventQueue.invokeLater(new Runnable(){
+            @Override
+            public void run(){                
+                DatabaseViewer a = new DatabaseViewer();
+                a.initComponents();
+
+                a.setVisible(true);
+                
+            }
+        });
     }
     
 }
